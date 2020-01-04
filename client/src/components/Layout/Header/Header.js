7ee10aa86ffa69807fc321a2d7888cb56392d5fe
Seongmin <lll2slll@naver.com>
2020-01-04 19:43:06 +0900

    Small header 적용

--- client/src/components/Layout/Header/Header.js
+++ client/src/components/Layout/Header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classNames from "classnames";
 import styles from "./Header.scss";
+import { getScrollTop } from "lib/common";
 import { Thumbnail } from "components/common";
 import Brand from "./Brand";
 import Search from "./Search";
@@ -18,23 +19,57 @@ const HeaderTitle = ({ title }) => (
   </div>
 );
 
-const Header = ({ background, thumbnail, title, onSearch }) => {
-  return (
-    <header
-      className={cx("header")}
-      style={{
-        backgroundImage: `url(${background ? background : backgroundImg})`
-      }}
-    >
-      <div className={cx("header-content")}>
-        <Thumbnail url={thumbnail} type="user" />
-        <Brand />
-        <HeaderTitle title={title} />
-        <Search onSearch={onSearch} />
-      </div>
-    </header>
-  );
-};
+class Header extends React.Component {
+  pos = 520;
+  element = React.createRef();
+
+  state = {
+    fixed: false
+  };
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.onScroll);
+  }
+
+  onScroll = () => {
+    const scrollTop = getScrollTop();
+    console.log(scrollTop, this.pos);
+
+    const fixed = scrollTop >= this.pos;
+    if (this.state.fixed !== fixed) {
+      this.setState({ fixed });
+    }
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll);
+  }
+
+  render() {
+    const { fixed } = this.state;
+    const { background, thumbnail, title, onSearch } = this.props;
+
+    return (
+      <header
+        className={cx(fixed ? "short-header" : "header")}
+        style={{
+          backgroundImage: `url(${background ? background : backgroundImg})`
+        }}
+        ref={this.element}
+      >
+        <div className={cx("header-content")}>
+          <Thumbnail url={thumbnail} type="user" />
+          <Brand />
+          <HeaderTitle title={title} />
+          <Search
+            className={cx(fixed ? "dark" : "light")}
+            onSearch={onSearch}
+          />
+        </div>
+      </header>
+    );
+  }
+}
 
 Header.propTypes = {
   background: PropTypes.string,
--- client/src/components/Tags/Tags.js
+++ client/src/components/Tags/Tags.js
@@ -1,55 +1,63 @@
-import React from 'react';
-import classNames from 'classnames/bind';
-import styles from './Tags.scss';
-import { getScrollTop } from 'lib/common';
-import { Tag } from 'components/common';
-import PropTypes from 'prop-types';
+import React from "react";
+import classNames from "classnames/bind";
+import styles from "./Tags.scss";
+import { getScrollTop } from "lib/common";
+import { Tag } from "components/common";
+import PropTypes from "prop-types";
 
 const cx = classNames.bind(styles);
 
 class Tags extends React.Component {
-    pos = 0;
-    element = React.createRef();
+  pos = 0;
+  element = React.createRef();
 
-    state = {
-        fixed: false
+  state = {
+    fixed: false
+  };
+
+  componentDidMount() {
+    this.pos =
+      this.element.current.getBoundingClientRect().top + getScrollTop() - 50;
+    window.addEventListener("scroll", this.onScroll);
+  }
+
+  onScroll = () => {
+    const scrollTop = getScrollTop();
+    const fixed = scrollTop >= this.pos;
+    if (this.state.fixed !== fixed) {
+      this.setState({ fixed });
     }
+  };
 
-    componentDidMount() {
-        this.pos = this.element.current.getBoundingClientRect().top + getScrollTop();
-        window.addEventListener('scroll', this.onScroll);
-    }
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll);
+  }
 
-    onScroll = () => {
-        const scrollTop = getScrollTop();
-        const fixed = scrollTop >= this.pos;
-        if(this.state.fixed !== fixed) {
-            this.setState({ fixed });
-        }
-    }
+  render() {
+    const { fixed } = this.state;
+    const { tags } = this.props;
 
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.onScroll);
-    }
-
-    render() {
-        const { fixed } = this.state;
-        const { tags } = this.props;
-
-        return (
-            <div className={cx(`tags ${fixed ? 'fixed' : 'default'}`)} ref={this.element}>
-                {tags.map((tag, index) => (<Tag key={index} to={`/search?keyword=${tag}`}>{tag}</Tag>))}
-            </div>
-        )
-    }
+    return (
+      <div
+        className={cx(`tags ${fixed ? "fixed" : "default"}`)}
+        ref={this.element}
+      >
+        {tags.map((tag, index) => (
+          <Tag key={index} to={`/search?keyword=${tag}`}>
+            {tag}
+          </Tag>
+        ))}
+      </div>
+    );
+  }
 }
 
 Tags.propTypes = {
-    tags: PropTypes.array
+  tags: PropTypes.array
 };
 
 Tags.defaultProps = {
-    tags: []
-}
+  tags: []
+};
 
 export default Tags;
